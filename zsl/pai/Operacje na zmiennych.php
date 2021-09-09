c538250c0ac3f30c0424a621833dbf6f445ddba3
JakubFranczak <89992665+JakubFranczak@users.noreply.github.com>
2021-09-09 19:05:14 +0200

    Add files via upload

---
+++ zsl/pai/Operacje na zmiennych.php
@@ -0,0 +1,26 @@
+<?php
+    echo PHP_VERSION; //7.4.8
+
+    echo '<br>', 2**10, '<br>';
+    $x=10;
+    $y=100;
+
+    echo $x<=>$y, "<br>"; //porównanie, potrójny operator porównania
+
+    //równe lub identyczne
+    $x=1;
+    $y=1.0;
+
+    if($x==$y){
+        echo "równe";
+    }else{
+        echo "różne";
+    }
+
+    //sprawdzenie dodatkowo typu danych
+    if($x===$y){
+        echo "równe";
+    }else{
+        echo "różne";
+    }
+?>
---
+++ zsl/pai/PAI2021.php
@@ -0,0 +1,57 @@
+<?php
+    echo "text<br>";
+    echo "text<br>";
+    $name="Krystyna";
+    echo 'Imię: $name<br>';
+    echo "Imię: $name<br>";
+
+    //typy danych
+    //całkowite
+    $calkowite=10;
+    $bin=0b1010;
+
+    echo $calkowite;
+    echo $bin;
+    
+    $oct=011;
+    echo $oct;
+
+    $hex=0x11;
+    echo $hex;
+
+    //zmiennoprzecinkowe
+    $x=10.5;
+    echo gettype($x);
+
+    //logiczna
+    $prawda=true;
+    $fałsz=false; //nie zapisujemy nazw zmiennych polskimi znakami
+
+    echo $prawda; //1
+    echo $fałsz; //nic nie wyswietli
+
+    //składnia heredoc
+    echo <<< E
+    <br><br>Imię: $name
+    E;
+
+    $tekst = <<< E
+    <br><br>Imię: $name
+    E;
+
+    echo $tekst;
+
+
+    //nowdoc
+    echo <<< 'E'
+    <br><br>Imię: $name<br><br>
+    E;
+
+    //konkatenacja
+    echo "tekst1"."tekst2<br>";
+
+    echo "Imię: \$name";
+
+
+
+?>
