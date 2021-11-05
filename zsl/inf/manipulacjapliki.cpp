1867f02c5ec62fe7019d14e6e8b2c7f35e2600c2
JakubFranczak <89992665+JakubFranczak@users.noreply.github.com>
2021-11-05 01:55:20 +0100

    Update manipulacjapliki.cpp

--- zsl/inf/manipulacjapliki.cpp
+++ zsl/inf/manipulacjapliki.cpp
@@ -3,25 +3,31 @@
 
 using namespace std;
 
-int main(){
+int main() {
     fstream f1;
     fstream f2;
-    f1.open("f1.txt",ios_base::out | ios_base::app);
+    fstream test;
+    test.open("f1.txt", ios_base::in);
+    int czyplik;
+    test >> czyplik;
+    test.close();
+    if(czyplik <0){
+    f1.open("f1.txt", ios_base::out | ios_base::app);
     int i = 0;
-    do{
+    do {
         f1 << i << endl;
         i++;
-    }while(i<21);
+    } while (i < 21);
     f1.close();
     f1.open("f1.txt", ios_base::in);
     int a;
-    f2.open("f2.txt",ios_base::out | ios_base::app);
+    f2.open("f2.txt", ios_base::out | ios_base::app);
 
-    while(f1>>a){
-       cout << a << endl;
-       if(a%7!=0){
-        f2 << a << endl;
-       }
+    while (f1 >> a) {
+        cout << a << endl;
+        if (a % 7 != 0) {
+            f2 << a << endl;
+        }
     }
     f2.close();
     f1.close();
@@ -29,15 +35,21 @@ int main(){
     f1.open("f2.txt", ios_base::in);
     float x = 0;
     int y = 0;
-    while(f1 >> a){
-        if(a%3!=0){
-        cout << a << endl;
-        x = x + a;
-        y++;
-       }
+    while (f1 >> a) {
+        if (a % 3 != 0) {
+            cout << a << endl;
+            x = x + a;
+            y++;
+        }
     }
     cout << "Suma wynosi: " << x << endl;
-    cout << "Srednia wynosi: " << x/y;
+    cout << "Srednia wynosi: " << x / y;
     f1.close();
     return 0;
+    }
+    else
+    {
+        cout << "Plik juz istnieje";
+        return 0;
+    }
 }
